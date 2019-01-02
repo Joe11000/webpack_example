ff5a3d6435921a0c06bbd9f8338661fca62ed51d
Joe Noonan <joenoonan27@gmail.com>
2019-01-02 16:16:32 -0600

    init

---
+++ app.js
@@ -0,0 +1,4 @@
+// import text  from './people.js'
+const text = require('./people.js')
+
+console.log(text)
---
+++ people.js
@@ -0,0 +1,4 @@
+// const text = 'Hello World'
+// export default text;
+
+module.exports = 'Hello World'
